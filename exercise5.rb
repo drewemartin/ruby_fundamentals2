1287d1b5dd479b6266983687356bea40ada4a5dc
Drew Martin <drew.e.martin@gmail.com>
2014-06-03 16:05:37 -0400

    modified and corrected ruby file exercise7

--- exercise5.rb
+++ exercise5.rb
@@ -1,5 +1,12 @@
+puts "Hello, what is the temperature in fahrenheit"
+temp = gets.chomp.to_i
+
+
 def f_to_c(f)
 	((f - 32) * (5 / 9.to_f)).to_i
 end
 
-puts "Hello, it is #{f_to_c(60)} degrees Celsius"
+ctemp = f_to_c(temp)
+
+puts "It is #{ctemp} degrees Celsius"
+
---
+++ exercise7.rb
@@ -0,0 +1,13 @@
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+
+students.each do |key, value|
+	puts "#{key}: #{value} students"
+end
+
+students[:cohort 4] 
+
+puts students
