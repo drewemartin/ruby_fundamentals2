81d2448b81626ecb7da6f144aa0c9f1535a71344
Drew Martin <drew.e.martin@gmail.com>
2014-06-03 17:18:37 -0400

    final edit of files and directory

--- exercise7.rb
+++ exercise7.rb
@@ -8,6 +8,22 @@ students.each do |key, value|
 	puts "#{key}: #{value} students"
 end
 
-students[:cohort 4] 
+students[:cohort4] = 43
+
+puts students.keys
+
+students.each do |key, value|
+	puts "with 5% more students ->" + " " + "#{key}: " + (value * 1.05).to_s
+end
+
+students.delete(students.keys[1])
 
 puts students
+
+total_students = 0
+students.values.each do |x|
+	total_students += x
+end 
+
+puts total_students
+
